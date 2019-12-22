e99861a3ecd23be18b27fb92efe46c24a3dc883b
Michael-MD <45169605+Michael-MD@users.noreply.github.com>
2019-12-22 19:20:43 +1100

    major improvements to the animation
    redesigned the way the animation is done to make it cleaner

--- plot_anim.js
+++ plot_anim.js
@@ -1,154 +1,148 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
 
-c_width = c.getClientRects()[0].width
-c_height = c.getClientRects()[0].height
 
-function coord_rect( loc ) { //[x,y]
-	return [ c_width/2+loc[0],c_height/2-loc[1] ]
+
+
+
+var canvas = document.createElement('canvas');	//make canvas
+canvas.id = "plot";	//give id
+canvas.width = window.innerWidth;	//make canvas browser width
+canvas.height = window.innerHeight; // make canvas browser height
+document.body.appendChild(canvas); //place in document
+var ctx = canvas.getContext("2d")	
+ctx.fillStyle = "#FF0000";	// lines in black
+
+x_p = window.innerWidth/100;	//percetange of screen
+y_p = window.innerHeight/100;	//pencentage of screen
+
+function rect_to_native(coord) {	//converts x,y coords to native coordes from top left corner
+	[x,y] = coord;
+	return [ x+window.innerWidth/2, -y+innerHeight/2 ];
 }
 
-function coord_rect_rev( loc ) { //[x,y]
-	return [ -c_width/2+loc[0],c_height/2+loc[1] ]
+function native_to_rect(coord) {	//converts x,y coords to native coordes from top left corner
+	[x,y] = coord;
+	return [ x-window.innerWidth/2, -(y-innerHeight/2) ];
 }
 
-function coord_polar( loc ) { //[x,y]
-	return coord_rect([ loc[0]*Math.cos(loc[1]), loc[0]*Math.sin(loc[1]) ]);
-}
+function circle(center,r) {
+	//convert to native
+	[x,y] = rect_to_native(center);
 
-//plot line give in cartesian
-function plt_line(loc_start, loc_finish, disp = [0,0], type="cart") {
-	if(type == "cart") {
-		loc_start = coord_rect(loc_start);	// convert coordinate to native coordinates
-		loc_finish = coord_rect(loc_finish);	// convert coordinate to native coordinates
-	}else if(type == "polar"){
-		loc_start = coord_polar(loc_start);	// convert coordinate to native coordinates
-		loc_finish = coord_polar(loc_finish);	// convert coordinate to native coordinates
-	}
-	
-
-	ctx.moveTo(loc_start[0]+disp[0],loc_start[1]+disp[1]);
-	ctx.lineTo(loc_finish[0]+disp[0],loc_finish[1]+disp[1]);
-	ctx.stroke();
-}
-
-
-
-
-
-
-mag = []
-freq = []
-
-
-pi = Math.PI/100;
-for(i=1;i<100;i+=2) {
-	mag.push( 4/(pi*i) );
-	freq.push( i ); 
-}
-
-
-
-/*mag = [4/pi,4/(3*pi),4/(5*pi),4/(7*pi),4/(9*pi),4/(11*pi),4/(13*pi),4/(15*pi)];
-freq = [1,3,5,7,9,11,13,15];*/
-
-
-
-displacement =[-250,0]
-
-
-var time = -10;
-
-
-function sum_x(arr, to) {	// sum arr up until and including to
-	var total_x = 0;
-	for(i=0; i<=to; i++) {
-		total_x+=arr[i]*Math.cos(time*freq[i]);
-	}
-
-	return total_x;
-}
-
-function sum_y(arr, to) {	// sum arr up until and including to
-	var total_y = 0;
-	for(i=0; i<=to; i++) {
-		total_y+=arr[i]*Math.sin(time*freq[i]);
-	}
-
-	return total_y;
-}
-
-
-function draw_circle(center, rad, disp = [0,0]) {
 	ctx.beginPath();
-	ctx.arc(center[0]+disp[0],center[1]+disp[1],rad, 0, 2*Math.PI);
+	ctx.arc(x,y,r,0,2*Math.PI);
+	ctx.stroke()
 }
 
+function line(start,finish) {
+	[a,b] = rect_to_native(start);
+	[x,y] = rect_to_native(finish);
+	ctx.moveTo(a,b);
+	ctx.lineTo(x,y);
+	ctx.stroke()
+}
 
-function draw(color = "#000000", width = 1){	//instead of clearing the canvas we can draw white lines on the original black lines
-	ctx.strokeStyle = color;
-	ctx.lineWidth = width;
-
-	prev=[0,0];
-	for(j=0;j<=mag.length;j++) {
-		plt_line( prev , [sum_x(mag,j),sum_y(mag,j)] ,displacement);
-		set = prev;
-		prev = coord_rect(prev);
-		
-		ctx.beginPath();
-		draw_circle([prev[0],prev[1]],mag[j],displacement);
-		
-		prev =  [sum_x(mag,j),sum_y(mag,j)];
+function sum(arr,ind) {
+	tot = 0;
+	for(i=0;i<ind;i++) {
+		tot+=arr[i];
 	}
 
-	ctx.strokeStyle = "#000000";
-	ctx.lineWidth = 1;
-	return set;
+	return tot;
 }
 
 
-trace = [sum_x(mag,2),0];
+function angle_m(re,im) {
+	if(re>0 && im>0) {return Math.atan(im/re);}
+	else if( re>0 && im <0 ) { return (Math.atan(im/re)); }
+	else if( re<0 && im > 0 ) {return (Math.PI + Math.atan(im/re)); }
+	else if(re<0 && im<0) { return (-Math.PI + Math.atan(im/re)); }
+	else if( re == 0 && im>0 ) {return Math.PI/2;}
+	else if( im ==0 && re >0 ) {return 0;}
+	else if( re == 0 && im<0 ) {return (-Math.PI/2);}
+	else if( im ==0 && re <0 ) {return Math.PI;}
+}
 
-amps = [];
+time = 0;
 
-function draw_plt() {
-	s = 0;
-	for(k=amps.length;k>=amps.length-1000;k--) {
-		if(k<0) {break;}
-			ctx.beginPath();
-			
-			plt_line([s,amps[k-1]],[s,amps[k]]);
-			ctx.stroke();
-			s++;
+freq = [];
+mag_im_0 = [];
+mag_re_0 = [];
+// ---------------- magnitudes ----------------
+
+
+
+for(j=1;j<50;j+=2) {
+	mag_im_0.push(0);
+	mag_re_0.push(4/(j*Math.PI));
+	freq.push(j);
+}
+
+mag = [];
+for(i=0;i<mag_re_0.length;i++) {
+	mag.push( Math.sqrt(mag_re_0[i]**2+mag_im_0[i]**2) );
+}
+summer =  mag.reduce((a, b) => a + b, 0);
+for(i=0; i<mag.length;i++) { 	//rescale such that total length is 1/10 screen width
+	mag[i] = (mag[i]/summer)*(window.innerWidth/10);
+}
+
+
+// ---------------- angular frequencies ----------------
+for(i=0;i<freq.length;i++) {
+	freq[i] = freq[i]/Math.min(...freq);	//fastest phasor completes 5 cycles/time, time is set by setInterval function
+}
+
+
+
+// ---------------- draw epicycles ----------------
+setInterval(draw,30);
+setInterval(clear,1000)
+
+tips = [];
+function draw() {
+	time+=0.1;	//time unit
+	ctx.clearRect(0, 0, canvas.width, canvas.height);	//clear canvas
+
+	mag_re = mag_re_0.slice(0,mag_re_0.length);	//copy by val not re !!
+	mag_im = mag_im_0.slice(0,mag_im_0.length);
+
+
+	for(i=0; i<mag.length;i++) { 
+		angle = angle_m(mag_re_0[i],mag_im_0[i]) + freq[i]*time;
+
+		mag_re[i] = mag[i]*Math.cos(angle); 
+		mag_im[i] = mag[i]*Math.sin(angle);
+	}	//assign lengths such that total length length is 1/3 of total window width
+
+
+
+
+	for(j=0;j<mag.length;j++) {
+		line( [ sum(mag_re,j),sum(mag_im,j) ], [ sum(mag_re,j)+mag_re[j],sum(mag_im,j)+mag_im[j] ])
+		circle( [ sum(mag_re,j),sum(mag_im,j) ], mag[j] );
 	}
-}
+	circle([0,0],0);	//clear bug of not deleting last drawn cirlce
 
-setInterval(myTimer, 30);
-function myTimer() {
-	ctx.beginPath();
-	
-	
-	/*we clear the canvas in this way as to preserve the overall drawing*/
-	//draw("#FFFFFF",2.5);	//clear canvas
-
-	ctx.clearRect(0, 0, c.width, c.height);
-	time+=0.05;
-	prev = draw();
-	/*ctx.beginPath();
-	ctx.arc(0,prev[1],100, 0, 2*Math.PI);
-	ctx.stroke();*/
-
-
-	
-	//trace shape
-	/*ctx.lineWidth = 10;
-	plt_line(trace,prev);
-	ctx.lineWidth = 1;
-	
-	trace = prev;*/
-	amps.push(prev[1]);
-
+	tips.push( ([ sum(mag_re,mag.length),sum(mag_im,mag.length) ]) );
 	draw_plt();
 }
 
 
+function draw_plt() {
+	// use tips to draw imaginary and real parts
+	line(tips[tips.length-1], [x_p*10,tips[tips.length-1][1]]);	// line from tip to im plot
+
+	t=x_p*10;
+	for(j=tips.length-1;j>0;j--) {	//im plot
+		line([t,tips[j][1]],[t,tips[j-1][1]]);
+		t+=1;
+	}
+
+
+
+}
+
+
+function clear() {
+	tips = tips.slice(tips.length-300, tips.length);
+}
